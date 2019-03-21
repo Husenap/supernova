cca0ecd44d744012e183a11eaa74b0d614694af8
Hussein Taher <hussein_taher@outlook.com>
2019-03-21 22:42:38 +0100

    update glfw.cmake

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -1,3 +1,16 @@
+if (WIN32)
+	set(VULKAN_LIBRARY "$ENV{VULKAN_SDK}/Lib/vulkan-1.lib")
+	set(VULKAN_INCLUDE_DIR "$ENV{VULKAN_SDK}/Include")
+endif()
+if (UNIX)
+	set(VULKAN_LIBRARY "$ENV{VULKAN_SDK}/lib/libvulkan.so")
+	set(VULKAN_INCLUDE_DIR "$ENV{VULKAN_SDK}/include")
+endif()
+
+message("-- Vulkan paths:")
+message("-- ${VULKAN_LIBRARY}")
+message("-- ${VULKAN_INCLUDE_DIR}")
+
 set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
@@ -5,23 +18,14 @@ set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
 
 add_subdirectory("third_party/glfw")
 
-if (WIN32)
-	set(VULKAN_LIB_PATH "$ENV{VULKAN_SDK}/Lib/vulkan-1.lib")
-	set(VULKAN_INC_PATH "$ENV{VULKAN_SDK}/Include")
-endif()
-if (UNIX)
-	set(VULKAN_LIB_PATH "$ENV{VULKAN_SDK}/lib/libvulkan.so")
-	set(VULKAN_INC_PATH "$ENV{VULKAN_SDK}/include")
-endif()
-
 set(gfx_libraries
 	"glfw"
-	"${VULKAN_LIB_PATH}")
+	"${VULKAN_LIBRARY}")
 
 set(gfx_includes
 	"${CMAKE_CURRENT_SOURCE_DIR}/third_party/glfw/include"
 	"${CMAKE_CURRENT_SOURCE_DIR}/third_party/glm"
 	"${CMAKE_CURRENT_SOURCE_DIR}/third_party/stb"
-	"${VULKAN_INC_PATH}")
+	"${VULKAN_INCLUDE_DIR}")
 
 set_target_properties("glfw" PROPERTIES FOLDER "third_party//GLFW")
