2af9b4f0999498e8677b407dde1312422b3f3770
Hussein Taher <hussein_taher@outlook.com>
2019-03-21 21:31:41 +0100

    updated vulkan paths

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -8,14 +8,23 @@ set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
 
 add_subdirectory("third_party/glfw")
 
+if (WIN32)
+	set(VULKAN_LIB_PATH "$ENV{VULKAN_SDK}/Lib/vulkan-1.lib")
+	set(VULKAN_INC_PATH "$ENV{VULKAN_SDK}/Include")
+endif()
+if (UNIX)
+	set(VULKAN_LIB_PATH "$ENV{VULKAN_SDK}/x86_64/lib/libvulkan.so")
+	set(VULKAN_INC_PATH "$ENV{VULKAN_SDK}/x86_64/include")
+endif()
+
 set(gfx_libraries
 	"glfw"
-	"$ENV{VULKAN_SDK}/Lib/vulkan-1.lib")
+	"${VULKAN_LIB_PATH}")
 
 set(gfx_includes
 	"${CMAKE_CURRENT_SOURCE_DIR}/third_party/glfw/include"
 	"${CMAKE_CURRENT_SOURCE_DIR}/third_party/glm"
 	"${CMAKE_CURRENT_SOURCE_DIR}/third_party/stb"
-	"$ENV{VULKAN_SDK}/Include")
+	"${VULKAN_INC_PATH}")
 
 set_target_properties("glfw" PROPERTIES FOLDER "third_party//GLFW")
