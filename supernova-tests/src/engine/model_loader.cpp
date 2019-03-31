234a50476b56ac699e73d03a189771cab85012d4
Hussein Taher <hussein_taher@outlook.com>
2019-03-31 21:23:51 +0200

    fixed test resource paths

--- supernova-tests/src/engine/model_loader.cpp
+++ supernova-tests/src/engine/model_loader.cpp
@@ -2,13 +2,13 @@
 
 #include "../precompiled.h"
 
-TEST(model_loader, load_model_invalid_filepath){
+TEST(model_loader, load_model_invalid_filepath) {
 	std::optional<snova::model_data> data = snova::model_loader::load_model("hej");
 
 	EXPECT_EQ(data.has_value(), false);
 }
 
-TEST(model_loader, load_model_cube){
+TEST(model_loader, load_model_cube) {
 	std::optional<snova::model_data> data = snova::model_loader::load_model("res/models/cube.fbx");
 
 	EXPECT_EQ(data.has_value(), true);
@@ -17,7 +17,7 @@ TEST(model_loader, load_model_cube){
 	EXPECT_EQ(data->m_indices.size(), 36);
 }
 
-TEST(model_loader, load_model_no_meshes){
+TEST(model_loader, load_model_no_meshes) {
 	std::optional<snova::model_data> data = snova::model_loader::load_model("res/models/no_meshes.fbx");
 
 	EXPECT_EQ(data.has_value(), false);
